fc25729209dedfd02c8678a3243c1c0f69d2944f
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-06-18 20:30:33 +0530

    new....

---
+++ offcanvas.js
@@ -0,0 +1,8 @@
+function offcanvas() {
+    var x = document.getElementById("offcanvas");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
