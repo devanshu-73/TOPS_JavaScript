ea360ea5a7bc4c3e9092f00c102f1bef1b9667a2
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-06-21 13:08:54 +0530

    New...

---
+++ allInone.js
@@ -0,0 +1,26 @@
+// var showBtn = document.getElementById('show-btn');
+// var showDiv = document.getElementById('show-div');
+
+// ------------------------------------------------------------------
+// function show() {
+//     console.log("Hello.....");
+// }
+// show();
+// ------------------------------------------------------------------
+
+// showBtn.onclick = function () {
+//     console.log("Hello.....");
+// }
+// ------------------------------------------------------------------
+
+// showDiv.innerHTML = "hello... Devanshu...";
+// ------------------------------------------------------------------
+
+
+// function show() {
+//     var a = document.getElementById('num-1').value;
+//     var b = document.getElementById('num-2').value;
+//     if (a > b) {
+//         console.log("running.....");
+//     }
+// }
