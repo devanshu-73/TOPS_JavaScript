011d95be6bdb326dcf2c6ea28d8b89f7900b5bf7
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-06-25 20:51:22 +0530

    Updated....

--- allInone.js
+++ allInone.js
@@ -1,6 +1,5 @@
 // var showBtn = document.getElementById('show-btn');
 // var showDiv = document.getElementById('show-div');
-
 // ------------------------------------------------------------------
 // function show() {
 //     console.log("Hello.....");
@@ -24,3 +23,44 @@
 //         console.log("running.....");
 //     }
 // }
+
+// --------------------------------------------------
+
+// for (let i = 1; i <= 10; i++) {
+//     document.write(i, "<br>");
+// }
+
+// --------------------------------------------------
+// let i = 1
+// while (i <= 10) {
+//     document.write(i, "<br>");
+//     i++
+// }
+// --------------------------------------------------
+// let i =1;
+// do{
+// document.write(i,"<br>");
+// i++;
+// }while (i<10)
+// --------------------------------------------------
+
+// let i = prompt("Enter Number....");
+// let j = i % 2;
+// switch (j) {
+//     case 0:
+//         document.write("Even");    
+//         break;
+
+//         default:
+//         document.write("odd");    
+//         break;
+// }
+// --------------------------------------------------
+const str = "Hello Dev";
+const str2 = "How r u? fine or not?";
+//  document.write(str.charAt(5));
+//  document.write(str.concat(str2));
+//  document.write(str2.indexOf("?"));
+document.write(str2.match());
+// console.log(str);
+// --------------------------------------------------
